5961c1698cc5dd1502ccfba72f37c8cab6da4656
Taishi <taishw@uw.edu>
2024-05-01 23:46:02 -0700

    Updating language

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,8 +10,8 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	author: 'Taishi Walden', // Site author
-	title: "Taishi's AI Toolbox", // Site title.
-	description: 'Join us for humorous insights, quirky tech trends, and a fresh perspective on AI through the eyes of the digital-native generation.', // Description to display in the meta tags
+	title: "Taishi's AI Repository", // Site title.
+	description: 'Humorous insights, quirky tech trends, and a fresh perspective on AI through the eyes of the digital-native generation.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
